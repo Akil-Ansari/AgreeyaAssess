c77af4fc58e42dece028e0a67340453571ec1ee3
Akil-Ansari <135803102+Akil-Ansari@users.noreply.github.com>
2024-03-29 10:15:40 +0530

    Add files via upload

---
+++ Account/Response.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Agreeya.Account
+{
+    public class Response
+    {
+        public Response() { }
+        public Response(int statusCode, string status, string message) 
+        {
+            Status = status;
+            Message = message;
+            StatusCode = statusCode;
+        }
+
+        public int StatusCode { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+       
+
+        public override string ToString()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            return JsonSerializer.Serialize(this, options);
+        }
+    }
+}
