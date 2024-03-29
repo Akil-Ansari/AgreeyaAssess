ee52be3c2722de51f8cdbb37f6192ea86bc17c1c
Akil-Ansari <135803102+Akil-Ansari@users.noreply.github.com>
2024-03-29 10:17:17 +0530

    Add files via upload

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,57 @@
+﻿using Agreeya.Authentication;
+using Agreeya.Data;
+using Agreeya.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agreeya.Controllers
+{
+    [Authorize(Roles = UserRoles.Admin)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IUnitOfWork uow;
+
+        public CustomerController(IUnitOfWork _uow)
+        {
+            this.uow = _uow;
+        }
+
+        [HttpGet("GetCustomers")]
+        public async Task<IActionResult> GetCustomers()
+        {
+            var cityList = await uow.CustomerRepository.GetCustomerAsync();
+            return Ok(cityList);
+        }
+
+        [HttpPost("addCustomer")]
+        public async Task<IActionResult> addCustomer(Customer customer)
+        {
+            uow.CustomerRepository.AddCustomer(customer);
+            await uow.SaveAsync();
+            return StatusCode(201);
+        }
+
+        [HttpPut("update/{id}")]
+        public async Task<IActionResult> UpdateCity(int id, Customer customer)
+        {
+            uow.CustomerRepository.UpdateCustomer(id, customer);
+            await uow.SaveAsync();
+            return StatusCode(200);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DelCity(int id)
+        {
+            uow.CustomerRepository.DeleteCustomer(id);
+            await uow.SaveAsync();
+            return Ok(id);
+        }
+    }
+}
---
+++ Controllers/MailController.cs
@@ -0,0 +1,30 @@
+﻿using Agreeya.Email;
+using Agreeya.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agreeya.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        private readonly IMailService _mailService;
+        //injecting the IMailService into the constructor
+        public MailController(IMailService MailService)
+        {
+            _mailService = MailService;
+        }
+
+        [HttpPost]
+        [Route("SendMail")]
+        public async Task<bool> SendMail(MailData mailData)
+        {
+            return await _mailService.SendMail(mailData);
+        }
+    }
+}
